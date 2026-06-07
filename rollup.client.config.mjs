0fa395ab7654f096d0187dd67497a950ba29e985
Alex Hoyau <a.hoyau@silexlabs.org>
2026-06-07 23:36:47 +0200

    chore: migrate from npm to yarn (CI + lockfile + remove broken eslint setup)
    - CI: replace npm with yarn in npm-publish.yml and test.yml
    - Replace package-lock.json with yarn.lock
    - Remove @rollup/plugin-eslint from rollup configs and devDeps: the package was unused (no eslint config file ever existed) and broke standalone build when yarn's strict hoisting did not satisfy the implicit eslint peer dep

--- rollup.client.config.mjs
+++ rollup.client.config.mjs
@@ -1,5 +1,4 @@
 import typescript from '@rollup/plugin-typescript';
-import eslint from '@rollup/plugin-eslint';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
@@ -14,7 +13,6 @@ export default {
   plugins: [
     typescript(), // Supports typescript
     nodeResolve(), // Import modules from node_modules
-    eslint(), // Linting
     commonjs(), // Convert CommonJS modules to ES6 when importing node_modules
   ],
 };
